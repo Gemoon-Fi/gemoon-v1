4ee8caf02c0212404d514876489eac368940f17d
Denis Davydov <DDavydov@scala-r.ru>
2025-09-13 02:53:25 +0300

    fix linter issues

--- test/PercentTest.sol
+++ test/PercentTest.sol
@@ -4,14 +4,14 @@ import "forge-std/Test.sol";
 import "../src/contracts/utils/Percent.sol";
 
 contract PercentTest is Test {
-    function testSubPercent() public {
+    function testSubPercent() external pure {
         assertEq(Percent.subPercent(100, 20), 80);
         assertEq(Percent.subPercent(100, 0), 100);
         assertEq(Percent.subPercent(0, 20), 0);
         assertEq(Percent.subPercent(1e18, 50), 5e17);
     }
 
-    function testAddPercent() public {
+    function testAddPercent() external pure {
         assertEq(Percent.addPercent(100, 20), 120);
         assertEq(Percent.addPercent(100, 0), 100);
         assertEq(Percent.addPercent(0, 20), 0);
